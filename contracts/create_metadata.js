f5afd520db9f37957d28064c780157c5c2b621b8
Sandro Chaoling <53383844+xchaolingx@users.noreply.github.com>
2022-12-13 03:55:43 -0300

    Delete contracts directory

--- contracts/create_metadata.js
+++
@@ -1,9 +0,0 @@
-/*
-import * as IPFS from 'ipfs-core'
-
-const ipfs = await IPFS.create()
-const { cid } = await ipfs.add('Hello world')
-console.info(cid)
-// QmXXY5ZxbtuYj6DnfApLiGstzPN7fvSyigrRee3hDWPCaf
-
-*/
