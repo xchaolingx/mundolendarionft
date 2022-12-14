7b4022e11fed4bd249650c105bed09f5fbdc323a
Sandro Chaoling <53383844+xchaolingx@users.noreply.github.com>
2022-12-14 14:33:19 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,11 @@
 function getNFTMetadata() {
-  // Obter o endereço da carteira do usuário e o ID da NFT
+  // Obter os valores dos campos de entrada da página
   const walletAddress = document.getElementById('wallet-address').value;
   const nftId = document.getElementById('nft-id').value;
 
   // Buscar os metadados da NFT
-  // (neste exemplo, supomos que existe uma função "fetchNFTMetadata" que realiza a busca)
   const metadata = fetchNFTMetadata(walletAddress, nftId);
 
-  // Exibir os metadados da NFT na página
-  const metadataElement = document.getElementById('nft-metadata');
-  metadataElement.textContent = JSON.stringify(metadata, null, 2);
+  // Exibir os metadados na página
+  document.getElementById('nft-metadata').innerText = JSON.stringify(metadata, null, 2);
 }
